757cda54754429c3be640cf638da8354794fe0a5
Ase444ka <istomina.asia@yandex.ru>
2025-10-20 17:22:53 +0300

    initial commit

---
+++ src/components/UsersForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container max-width="900">
+
+      <div class="mb-8 text-h5">
+        Учетные записи
+        <v-btn icon="mdi-plus"  class="ml-2 text-h5"></v-btn>
+      </div>
+
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>

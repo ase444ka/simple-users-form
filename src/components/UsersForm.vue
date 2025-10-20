013139cfa2f372946a2677d591ae4f335de630e9
Ase444ka <istomina.asia@yandex.ru>
2025-10-20 18:59:56 +0300

    add labels

--- src/components/UsersForm.vue
+++ src/components/UsersForm.vue
@@ -6,7 +6,15 @@
         <v-btn icon="mdi-plus"  class="ml-2 text-h5"></v-btn>
       </div>
       <v-alert  icon="mdi-help-circle">Для указания нескольких меток для одной пары используйте разделитель ;</v-alert>
+      <v-row class="mt-4 text-disabled">
+        <v-col>Метки</v-col>
+        <v-col>Тип записи</v-col>
+        <v-col>Логин</v-col>
+        <v-col>Пароль</v-col>
+      </v-row>
+      <v-row>
 
+      </v-row>
   </v-container>
 </template>
 

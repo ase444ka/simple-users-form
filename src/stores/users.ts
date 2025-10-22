b50304d1a1cfe13c83968a9bd1831ee3f96cb207
Ase444ka <istomina.asia@yandex.ru>
2025-10-22 11:43:30 +0300

    complete store

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,36 +1,64 @@
-import {ref} from 'vue';
-import {defineStore} from 'pinia';
-import storage from '../utils/localStorage'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import storage from "../utils/localStorage";
 
-type user = {
+type User = {
   id?: number;
   name: string;
   password: string | null;
-  type: 'LDAP' | 'local';
-  tags: {text: string}[]
-}
+  type: "LDAP" | "local";
+  tags: { text: string }[];
+};
 
-const cashedUsers = storage.get('users') || []
-const lastIndex = +storage.get('lastUserIndex') || 1
+const cashedUsers: User[] = storage.get("users") || [];
+const lastIndex: number = +storage.get("lastUserIndex") || 1;
+const isValid = (user: User) => {
+  if (user.tags.map((t) => t.text).join(";").length > 50) {
+    return false;
+  }
+  if (!user.name || user.name.length > 100) {
+    return false;
+  }
+  if (
+    (user.type === "local" && !user.password) ||
+    (user.password && user.password.length > 100)
+  ) {
+    return false;
+  }
+};
 
-export const useUserStore = defineStore('users', () => {
-  const users = ref<user[]>(cashedUsers);
-  const lastUserIndex = ref<number>(lastIndex)
+export const useUserStore = defineStore("users", () => {
+  const users = ref<User[]>(cashedUsers);
+  const lastUserIndex = ref<number>(lastIndex);
 
   function createUser() {
-    users.value.push({name: '', password: null, type: 'LDAP', tags: []})
+    users.value.push({ name: "", password: null, type: "LDAP", tags: [] });
   }
 
   function addUser() {
-    const newUser = users.value.pop()
-    users.value.push({...newUser!, id: ++lastUserIndex.value})
-    storage.set('users',users.value)
-    storage.set('lastUserIndex', lastUserIndex.value)
+    const newUser = users.value.pop();
+    users.value.push({ ...newUser!, id: ++lastUserIndex.value });
+    storage.set("users", users.value);
+    storage.set("lastUserIndex", lastUserIndex.value);
   }
+
+  function saveUser() {
+    storage.set("users", users.value);
+  }
+
+  function deleteUser(id: number) {
+    users.value = users.value.filter((user) => user.id !== id);
+    storage.set("users", users.value);
+  }
+
+  const isAllOk = computed(() => users.value.every((user) => isValid(user)));
 
   return {
     users,
     createUser,
-    addUser
+    addUser,
+    saveUser,
+    deleteUser,
+    isAllOk,
   };
 });
